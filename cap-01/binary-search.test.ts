77baf09f5365d1c0d6ba152727bac01ee31024b1
henrfarias <henrique.biasibetti@gmail.com>
2022-10-29 16:44:16 -0300

    feat: initial commit

---
+++ cap-01/binary-search.test.ts
@@ -0,0 +1,7 @@
+import { binarySearch } from './binary-search'
+
+describe('Binary search', () => {
+  it('should return true', () => {
+    expect(binarySearch()).toBeTruthy()
+  })
+})
---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  bail: 0,
+  cacheDirectory: "/tmp/jest_rs",
+  clearMocks: true,
+  preset: 'ts-jest',
+  collectCoverage: false,
+  coveragePathIgnorePatterns: [
+    "/node_modules/"
+  ],
+  fakeTimers: {
+    "enableGlobally": false
+  },
+  maxWorkers: "50%",
+  // modulePathIgnorePatterns: [],
+  // resetMocks: true,
+  restoreMocks: true,
+  verbose: false,
+  transform: {}
+};
